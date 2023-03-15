9e3d6748f930ad199f7a42899600585db3074889
Jafar Thwahrah <jaffardawahreh2@gmail.com>
2023-03-15 04:48:35 +0300

    setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Experience from "./components/Experience";
+import { Canvas } from "@react-three/fiber";
+function App() {
+  return (
+    <div className="App">
+      <Canvas>
+        <color attach="background" args={["#101010"]} />
+        <fog attach="fog" args={["#101010", 10, 20]} />
+
+        <Experience />
+      </Canvas>
+    </div>
+  );
+}
+
+export default App;
