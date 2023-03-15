76d41d3cdd543f227404f09474fd79ee7f68bbd7
Jafar Thwahrah <jaffardawahreh2@gmail.com>
2023-03-15 17:23:29 +0300

    edit reflection position

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -7,35 +7,34 @@ import { Suspense } from "react";
 import Chair from "./Chair";
 const Experience = () => {
   return (
-    <perspectiveCamera position={[0, 0, -1]}>
-      <PresentationControls
-        speed={1.5}
-        global
-        zoom={0.7}
-        polar={[-0.1, Math.PI / 4]}
-      >
-        <Stage environment={"city"} intensity={1} contactShadow={false}>
-          <Suspense fallback={null}>
-            <Chair />
-          </Suspense>
-        </Stage>
-        <mesh position={[0, -1.53, 0]} rotation={[-Math.PI / 2, 0, 0]}>
-          <planeGeometry args={[170, 170]} />
-          <MeshReflectorMaterial
-            blur={[300, 100]}
-            resolution={2048}
-            mixBlur={1}
-            mixStrength={40}
-            roughness={1}
-            depthScale={1.2}
-            minDepthThreshold={0.4}
-            maxDepthThreshold={1.4}
-            color="#101010"
-            metalness={0.5}
-          />
-        </mesh>
-      </PresentationControls>
-    </perspectiveCamera>
+    <PresentationControls
+      speed={1.5}
+      global
+      zoom={0.7}
+      polar={[-0.1, Math.PI / 4]}
+    >
+      <Stage environment={"city"} intensity={1} contactShadow={false}>
+        <Suspense fallback={null}>
+          <Chair />
+        </Suspense>
+      </Stage>
+
+      <mesh rotation={[-Math.PI / 2, 0, 0]}>
+        <planeGeometry args={[170, 170]} />
+        <MeshReflectorMaterial
+          blur={[300, 100]}
+          resolution={2048}
+          mixBlur={1}
+          mixStrength={40}
+          roughness={1}
+          depthScale={1.2}
+          minDepthThreshold={0.4}
+          maxDepthThreshold={1.4}
+          color="#101010"
+          metalness={0.5}
+        />
+      </mesh>
+    </PresentationControls>
   );
 };
 export default Experience;
